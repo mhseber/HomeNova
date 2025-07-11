5017bf635093f52531ea10017f9b294f31ff77ec
mahamudul hasan seber <sebermh@gmail.com>
2025-07-12 02:08:26 +0600

    Video Section Update Code

--- src/app/Pages/Video.jsx
+++ src/app/Pages/Video.jsx
@@ -1,7 +1,14 @@
+import { FaGraduationCap } from "react-icons/fa";
+import { GiCutDiamond } from "react-icons/gi";
+import { IoTrophySharp } from "react-icons/io5";
+import { FaFighterJet } from "react-icons/fa";
+import { TiWorld } from "react-icons/ti";
+import { MdPriceChange } from "react-icons/md";
+import { MdOutlineRoundaboutRight } from "react-icons/md";
 const Video = () => {
   return (
     <div className="p-5 sm:p-10">
-      <div className="mt-10  bg-[#aab1c6] shadow-2xl  rounded-xl">
+      <div className="mt-10  bg-[#c3c9db] shadow-2xl  rounded-xl">
         {/* Video + Form section */}
         <div className="flex flex-col-reverse items-center justify-between gap-10 p-5 lg:flex-row">
           {/* Form */}
@@ -87,11 +94,63 @@ const Video = () => {
         {/* CHOOSE icons section */}
         <section className="px-4 py-10">
           <div className="flex items-center gap-4">
-            <div className="flex-grow border-t-4 border-black" />
+            <div className="flex-grow border-t-2 border-black" />
             <span className="text-xl font-semibold whitespace-nowrap">
               WHY TO CHOOSE US
             </span>
-            <div className="flex-grow border-t-4 border-black" />
+            <div className="flex-grow border-t-2 border-black" />
+          </div>
+          {/* icons + text */}
+          <section className="flex flex-wrap justify-center gap-20 py-10 text-center">
+            <div>
+              <FaGraduationCap className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                30 YEARS OF <br />
+                EXPERIENCE
+              </p>
+            </div>
+            <div>
+              <GiCutDiamond className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                EXCELLENT <br />
+                SERVICE
+              </p>
+            </div>
+            <div>
+              <IoTrophySharp className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                TOP <br />
+                QUALITY
+              </p>
+            </div>
+            <div>
+              <FaFighterJet className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                FAST <br />
+                SOLUTIONS
+              </p>
+            </div>
+            <div>
+              <TiWorld className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                WORLDWIDE <br />
+                DELIVERY
+              </p>
+            </div>
+            <div>
+              <MdPriceChange className="mx-auto text-7xl" />
+              <p className="mt-2 font-semibold">
+                BEST PRICE <br />
+                GUARANTEE
+              </p>
+            </div>
+          </section>
+          {/* Button */}
+          <div className="mt-5 text-center">
+            <button className="btn bg-[#555879] border-none text-white">
+              <MdOutlineRoundaboutRight />
+              ABOUT OUR COMPANY
+            </button>
           </div>
         </section>
       </div>
