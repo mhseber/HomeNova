b927521d081f49a242fbc92d2b628781cf61c8e7
mahamudul hasan seber <sebermh@gmail.com>
2025-07-24 15:02:26 +0600

    Update Code

--- src/app/Pages/Testimonials.jsx
+++ src/app/Pages/Testimonials.jsx
@@ -21,45 +21,50 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <section className="my-20  bg-[#ebedf4] shadow-2xl  rounded-xl">
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-        spaceBetween={50}
-        slidesPerView={1}
-      >
-        {reviews.map((review) => (
-          <SwiperSlide key={review._id}>
-            <div className="flex flex-col items-center mx-24 my-16">
-              <Rating
-                style={{ maxWidth: 180 }}
-                value={review.rating}
-                readOnly
-              />
-              {/* Image Section */}
-              {review.img && (
-                <div className="pt-3">
-                  <Image
-                    src={review.img}
-                    alt={review.name}
-                    width={64}
-                    height={64}
-                    className="border-2 border-gray-600 rounded-full"
-                  />
-                </div>
-              )}
-              <p className="pt-3 font-semibold text-gray-700">
-                <span className="text-black">From : </span>
-                {review.location}
-              </p>
-              <p className="py-8 text-center">{review.details}</p>
-              <h3 className="text-2xl text-orange-400">{review.name}</h3>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
+    <div>
+      <h1 className="my-10 text-3xl font-light text-center">
+        Our Customers Say
+      </h1>
+      <section className="my-10  bg-[#ebedf4] shadow-2xl  rounded-xl">
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+          spaceBetween={50}
+          slidesPerView={1}
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col items-center mx-24 my-16">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+                {/* Image Section */}
+                {review.img && (
+                  <div className="pt-3">
+                    <Image
+                      src={review.img}
+                      alt={review.name}
+                      width={64}
+                      height={64}
+                      className="border-2 border-gray-600 rounded-full"
+                    />
+                  </div>
+                )}
+                <p className="pt-3 font-semibold text-gray-700">
+                  <span className="text-black">From : </span>
+                  {review.location}
+                </p>
+                <p className="py-8 text-center">{review.details}</p>
+                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </div>
   );
 };
 
