6c37cd081f3ce933895dbecaf3010c852d5290ba
mahamudul hasan seber <sebermh@gmail.com>
2025-07-29 22:06:51 +0600

    Footer Section Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com"], // ✅ তুমি যেই external image ইউজ করছো, সেই domain
+    domains: ["cdn-icons-png.flaticon.com"], 
   },
 };
 
